00ded4334aa124b6396a56d67809a649a17da310
Sindereve_Argo <sindereve850live@gmail.com>
2024-04-23 23:27:23 +0500

    23.04 много всего

--- Inspection/MainPage.xaml.cs
+++ Inspection/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MaterialDesignThemes.Wpf;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static MaterialDesignThemes.Wpf.Theme.ToolBar;
+
+
 
 namespace Inspection
 {
@@ -24,9 +28,53 @@ namespace Inspection
         {
             InitializeComponent();
         }
+
+        //Выход в окно авторизации
+        private void ExitInAvtori_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AvtoriPage()); ;
+        }
+
+
+        //Обработка событий из listBoxItem
+        private void ListBoxItem_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Получаем ListBoxItem, который вызвал событие
+            ListBoxItem item = sender as ListBoxItem;
+
+            // Получаем TextBlock внутри ListBoxItem
+            TextBlock textBlock = item.Content as TextBlock;
+
+            // Проверяем, был ли нажат TextBlock
+            if (textBlock != null)
+            {
+                // Получаем текст из TextBlock
+                string text = textBlock.Text;
+
+                // Выполнение действий в зависимости от текста
+                switch (text)
+                {
+                    case "Выход из пользователя":
+                        // Действия для "Выход из пользователя"
+                        ExitInAvtori_Click(sender, e);
+                        break;
+                    case "Выход":
+                        // Действия для "Выход"
+                        ExitButton_Click(sender, e);
+                        break;
+                    default:
+                        // Действия по умолчанию
+                        break;
+                }
+            }
+        }
+
+        //Выход из приложения
         private void ExitButton_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
+
+
     }
 }
